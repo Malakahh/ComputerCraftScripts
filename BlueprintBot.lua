334068ad1be60b8cfc88efaf043bb481f76b9f7c
Malakahh <malakahh@users.noreply.github.com>
2018-12-05 21:21:33 +0100

    initial scripts

---
+++ BlueprintBot.lua
@@ -0,0 +1,274 @@
+-- Note: All coordinates and directions are local to the turtle, and does not reflect cardinal directions
+-- Turtles always move one layer above where they are building
+
+local blueprint = { -- Blueprint
+	[0] = {
+		["successive"] = 1,
+		["pattern"] = { -- Pattern
+			-- 	 x:
+			-- z: 
+		}
+	}
+}
+
+local fuelSlot = 16
+local lowFuelThreshold = 100
+local facingDirection = "East"
+
+local patternMaterials = {}
+local pos = {
+	["x"] = 0,
+	["y"] = 0,
+	["z"] = 0
+}
+
+local function ValidatePatterns()
+	local numPatterns = #blueprint
+
+	local patternIdx
+	for patternIdx = 0, numPatterns, 1 do
+		local pattern = blueprint[patternIdx]["pattern"]
+		local maxZ = #pattern
+
+		local z
+		for z = 0, maxZ, 1 do
+			local maxX = #pattern[z]
+
+			local x
+			for x = 0, maxX, 1 do
+				local inventoryIdx = pattern[z][x]
+
+				if inventoryIdx ~= 0 then
+					local data = turtle.getItemDetail(inventoryIdx)
+
+					if data == nil then
+						return false
+					end
+
+					if patternMaterials[inventoryIdx] == nil then
+						patternMaterials[inventoryIdx] = data.name
+					elseif patternMaterials[inventoryIdx] ~= data.name then
+						return false
+					end
+				end
+			end
+		end 
+	end
+
+	return true
+end
+
+local function doRefuel()
+	local currentLevel = turtle.getFuelLevel()
+
+	if currentLevel == lowFuelThreshold then
+		turtle.select(fuelSlot)
+		turtle.refuel()
+	end
+end
+
+local function forward()
+	doRefuel()
+	turtle.forward()
+	if facingDirection == "East" then
+		pos.x = pos.x + 1
+	elseif facingDirection == "West" then
+		pos.x = pos.x - 1
+	elseif facingDirection == "South" then
+		pos.y = pos.y + 1
+	elseif facingDirection == "North" then
+		pos.y = pos.y - 1
+	end
+end
+
+local function back()
+	turtle.back()
+	if facingDirection == "East" then
+		pos.x = pos.x - 1
+	elseif facingDirection == "West" then
+		pos.x = pos.x + 1
+	elseif facingDirection == "South" then
+		pos.y = pos.y - 1
+	elseif facingDirection == "North" then
+		pos.y = pos.y + 1
+	end
+end
+
+local function up()
+	turtle.up()
+	pos.z = pos.z + 1
+end
+
+local function down()
+	turtle.down()
+	pos.z = pos.z - 1
+end
+
+local function turnRight()
+	turtle.turnRight()
+	if facingDirection == "East" then
+		facingDirection = "South"
+	elseif facingDirection == "South" then
+		facingDirection = "West"
+	elseif facingDirection == "West" then
+		facingDirection = "North"
+	elseif facingDirection == "North" then
+		facingDirection = "East"
+	end
+end
+
+local function turnLeft()
+	turtle.turnLeft()
+	if facingDirection == "East" then
+		facingDirection = "North"
+	elseif facingDirection == "North" then
+		facingDirection = "West"
+	elseif facingDirection == "West" then
+		facingDirection = "South"
+	elseif facingDirection == "South" then
+		facingDirection = "East"
+	end
+end
+
+local function FaceTowards(direction)
+	while facingDirection ~= direction then
+		turnRight()
+	end
+end
+
+local function MoveToCoord(x, y, z)
+	if x ~= nil and pos.x ~= x then
+		if pos.x > x then
+			FaceTowards("West")
+		elseif pos.x < x then
+			FaceTowards("East")
+		end
+
+		while pos.x ~= x do
+			forward()
+		end
+	end
+
+	if y ~= nil and pos.y ~= y then
+		if pos.y > y then
+			FaceTowards("North")
+		elseif pos.y < y then
+			FaceTowards("South")
+		end
+
+		while pos.y ~= y do
+			forward()
+		end
+	end
+
+	if z ~= nil and pos.z ~= z then
+		while pos.z ~= z do
+			if pos.z > z then
+				down()
+			elseif pos.z < z then
+				up()
+			end			
+		end
+	end
+end
+
+local function BuildOne(idxToPlace)
+	local blockAhead = turtle.inspect()
+	if blockAhead then
+		turtle.dig()
+	end
+
+	forward()
+
+	local blockBelow = turtle.inspectDown()
+	if blockBelow then
+		turtle.digDown()
+	end
+
+	turtle.select(idxToPlace)
+	turtle.placeDown()
+end
+
+local function ValidateInventory()
+	local i
+	for i = 1, 16, 1 do
+		local data = turtle.getItemDetail(i)
+		if data ~= nil then
+			if patternMaterials[i] ~= data.name then
+				return false
+			end
+		end
+	end
+
+	return true
+end
+
+local function ConstructPattern(pattern)
+	MoveToCoord(0, 0)
+
+	local maxZ = #pattern
+	local z
+	for z = 0, maxZ, 1 do
+		local startX, endX, increment
+
+		if z % 2 == 0 then
+			startX = 0
+			endX = #pattern[z]
+			increment = 1
+
+			FaceTowards("East")
+		else
+			startX = #pattern[z]
+			endX = 0
+			increment = -1
+
+			FaceTowards("West")
+		end
+
+		local x
+		for x = startX, endX, increment do
+			
+			local idxToPlace = pattern[z][x]
+
+			if not ValidateInventory() then
+				return false
+			end
+
+			BuildOne(idxToPlace)
+		end
+
+		if z + 1 ~= maxZ then
+			FaceTowards("South")
+
+			local idxToPlace = pattern[z + 1][x]
+
+			if not ValidateInventory() then
+				return false
+			end
+
+			BuildOne(idxToPlace)
+		end
+	end
+
+	return true
+end
+
+local function BuildBlueprint(layers)
+	if ValidatePatterns() then
+		local i
+		while true do
+			local patternIdx
+			for patternIdx = 0, #blueprint, 1 do
+				local successiveIdx
+				for successiveIdx = 1, blueprint[patternIdx]["successive"], 1 do
+					ConstructPattern(blueprint[patternIdx]["pattern"])
+					i = i + 1
+
+					if i == layers then
+						return
+					end
+				end
+			end
+		end
+	end
+end
---
+++ Updater.lua
@@ -0,0 +1,56 @@
+-- Title:		Updater
+-- Version:		1.1
+-- Author:		Malakahh
+-- All Rights Reserved
+
+local files = {
+	Updater = "",
+	
+}
+
+local function WriteToFile(name, content)
+	local file = fs.open(name, "w")
+	file.write(content)
+	file.close()
+end
+
+local function UpdateFile(file)
+	local url = files(tostring(file))
+	http.request(url)
+
+	local requesting = true
+	while requesting do
+		local event, _, handler = os.pullEvent()
+
+		if event == "http_success" then
+			local text = sourceText.readAll()
+
+			print("File successfully gotten. Writing to file: " .. file)
+
+			WriteToFile(file, text)
+			requesting = false;
+			return true
+		elseif event == "http_failure" then
+			print("Unable to connect to " .. url)
+			requesting = false;
+			return false
+		end
+	end
+end
+
+--- Start ---
+print("*** Updater ***")
+print("Please enter command:")
+local userInput = io.read()
+if userInput == "help" then
+	print("- \"help\":\tShows this message")
+	print("- \"list\":\tLists all available files")
+	print("- <file>:\tThe file to update")
+elseif userInput == "list" then
+	print("Found the following files:")
+	for k,_ in pairs(files) do
+		print(k)
+	end
+elseif UpdateFile(userInput) == false then -- attempts to update a single file. 
+	print("Error: wrong command or file doesn't exist. Use the \"help\"-command to see all available commands.")
+end
